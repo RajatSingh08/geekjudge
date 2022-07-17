eecf47c7ae33ea251f9b6b4b46c1a30ef1d4d409
RajatSingh08 <rajatsingh0805@gmail.com>
2022-07-17 15:40:30 +0530

    Add first_name & last_name in User Registration

--- OnlineJudge/OJ/forms.py
+++ OnlineJudge/OJ/forms.py
@@ -7,4 +7,5 @@ from django import forms
 class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'first_name', 'last_name',
+                  'email', 'password1', 'password2']
