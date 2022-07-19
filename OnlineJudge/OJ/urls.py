57902ec3e44aad2917d0f1ff163ac1c512c3dc41
RajatSingh08 <rajatsingh0805@gmail.com>
2022-07-20 00:01:36 +0530

    Connect description page to verdict page

--- OnlineJudge/OJ/urls.py
+++ OnlineJudge/OJ/urls.py
@@ -1,15 +1,16 @@
 from django.urls import path
 from . import views
 
-# app_name = 'OJ'
 urlpatterns = [
     path('register/', views.registerPage, name='register'),
     path('login/', views.loginPage, name='login'),
     path('logout/', views.logoutPage, name='logout'),
 
-    path('', views.homePage, name='dashboard'),
+    path('', views.dashboardPage, name='dashboard'),
     path('problems/', views.problemPage, name='problems'),
     path('problems/<int:problem_id>/', views.descriptionPage, name='description'),
+    path('problems/<int:problem_id>/verdict/',
+         views.verdictPage, name='verdict'),
     path('submissions/', views.submissionPage, name='submissions'),
-    path('leaderboard/', views.leaderboardPage, name='leaderboard')
+    path('leaderboard/', views.leaderboardPage, name='leaderboard'),
 ]
