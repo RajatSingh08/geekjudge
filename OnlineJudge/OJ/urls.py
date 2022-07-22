1e64a8359695543985e3d98990ed0e1ed6124a13
RajatSingh08 <rajatsingh0805@gmail.com>
2022-07-23 04:45:01 +0530

    Remove Bugs

---
+++ OnlineJudge/OJ/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('register/', views.registerPage, name='register'),
+    path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutPage, name='logout'),
+
+    path('', views.dashboardPage, name='dashboard'),
+    path('problems/', views.problemPage, name='problems'),
+    path('problems/<int:problem_id>/', views.descriptionPage, name='description'),
+    path('problems/<int:problem_id>/verdict/',
+         views.verdictPage, name='verdict'),
+    path('submissions/', views.submissionPage, name='submissions'),
+    path('leaderboard/', views.leaderboardPage, name='leaderboard'),
+]
---
+++ OnlineJudge/static/javascript/sort.js
@@ -0,0 +1,3 @@
+$(document).ready(function () {
+    $('#problem_list').DataTable();
+});
