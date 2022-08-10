e0d7905559ec1c7250baf09e68231bb9a33cc17d
RajatSingh08 <rajatsingh0805@gmail.com>
2022-08-10 22:20:16 +0530

    Update settings.py

--- GeekJudge/settings.py
+++ GeekJudge/settings.py
@@ -124,14 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
 MEDIA_URL = '/images/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 # Default primary key field type
