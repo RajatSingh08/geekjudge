81274735ac607bdb9f703e2f7ec22dca7b6ea561
RajatSingh08 <rajatsingh0805@gmail.com>
2022-08-20 00:22:34 +0530

    Production ready!

--- GeekJudge/settings.py
+++ GeekJudge/settings.py
@@ -126,7 +126,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [BASE_DIR / 'static']
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 # Default primary key field type
